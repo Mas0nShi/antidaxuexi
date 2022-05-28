039fed12d9960ef537443db1b5658b23290abdc7
Mas0nShi <fishilir@gmail.com>
2022-05-28 21:29:34 +0800

    auto publish

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools
 
 setuptools.setup(
     name="antidxx",
-    version="0.0.1",
+    version="0.0.2",
     description="fast to generate homeworks notices.",
     author="Mas0n",
     author_email="fishilir@gmail.com",
