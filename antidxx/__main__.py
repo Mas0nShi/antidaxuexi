a6353e58f917bd36e206145c30d844edf58863e1
Mas0nShi <fishilir@gmail.com>
2022-05-28 21:13:36 +0800

    项目重构

---
+++ antidxx/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+@Author: Mas0n
+@File: __main__.py
+@Time: 2022/5/28 21:06
+@Desc: It's all about getting better.
+"""
+from antidxx import generate
+
+
+def main():
+    print(generate(stdout=False))
+
+
+if __name__ == '__main__':
+    print(generate(stdout=False))
