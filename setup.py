a43cf71e95a56970db8c52fc55f0048f31f6af4c
Mas0nShi <fishilir@gmail.com>
2023-01-17 17:33:38 +0800

    🐛 rollback & auto publish pypi packages.

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@
 @Time: 2022/5/28 21:01
 @Desc: It's all about getting better.
 """
+import os
+import loguru
 import setuptools
+
+version = open(".version", "r").read()
+loguru.logger.info(f"😊 Publish Python Package Version: {version}")
 
 setuptools.setup(
     name="antidxx",
-    version="0.0.1",
+    version=version,
     description="fast to generate homeworks notices.",
     author="Mas0n",
     author_email="fishilir@gmail.com",
@@ -20,6 +25,7 @@ setuptools.setup(
         "beautifulsoup4",
         "lxml"
     ],
+    include_package_data=True,
     keywords="青春浙江 大学习 daxuexi qczj answers",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -31,7 +37,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    packages=setuptools.find_packages(where='antidxx', exclude=(), include=('*',)),
+    packages=setuptools.find_packages(where='.', exclude=(), include=('*',)),
     package_data={
         "antidxx.template": ["message.txt", "sections.txt", "staffs.txt"]
     },
